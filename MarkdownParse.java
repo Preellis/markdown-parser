23b77319eab66325dba78f61eee3cb8d3e65d50d
Preellis <prestone03@gmail.com>
2022-04-15 17:07:40 -0700

    Fixed Breaking Test with Brackets but no Parens

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -20,6 +20,10 @@ public class MarkdownParse {
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
+            if (closeParen == -1)
+            {
+                break;
+            }
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
             
